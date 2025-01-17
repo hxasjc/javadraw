e3d90fee007e1723c2a8b967d7792be93a922d01
Karl <karlvonweyhe@gmail.com>
2025-01-17 18:16:53 -0500

    replace lombok constructor with normal constructor

--- src/javadraw/Transform.java
+++ src/javadraw/Transform.java
@@ -1,15 +1,17 @@
 package javadraw;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-
 /**
  * Represents the "Transform" of an Object. It's "width", "height", and "angle" (or rotation).
  */
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Transform {
 
     protected double width, height, angle;
+
+    protected Transform(double width, double height, double angle) {
+        this.width = width;
+        this.height = height;
+        this.angle = angle;
+    }
 
     /**
      * Get the width of the Transform
